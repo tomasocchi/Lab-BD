6b6bf0b6b0526df2b91b4e356b23c4689ec08c2f
agosaddesso <145625018+agosaddesso@users.noreply.github.com>
2026-08-31 16:45:51 -0300

    Add new domains for esquema_grupo1_alt

--- tp1/dominiosPostgreSQL.sql
+++ tp1/dominiosPostgreSQL.sql
@@ -1,3 +1,4 @@
+-- primer esquema
 CREATE DOMAIN esquema_grupo1.domtipodoc AS VARCHAR(3)
    CHECK (VALUE IN ('DNI', 'PAS', 'CI', 'LC'))
    NOT NULL DEFAULT 'DNI';
@@ -27,3 +28,35 @@ CREATE DOMAIN esquema_grupo1.inicioReserva AS DATE
  
 CREATE DOMAIN esquema_grupo1.edad AS DATE
    CHECK (EXTRACT(YEAR FROM AGE(CURRENT_DATE, VALUE)) >= 21);
+
+-- segundo esquema
+CREATE DOMAIN esquema_grupo1_alt.domtipodoc AS VARCHAR(3)
+   CHECK (VALUE IN ('DNI', 'PAS', 'CI', 'LC'))
+   NOT NULL DEFAULT 'DNI';
+ 
+CREATE DOMAIN esquema_grupo1_alt.domTipoHabitacion AS VARCHAR(20)
+   CHECK (VALUE IN ('Simple', 'Doble', 'Suite', 'Twin', 'Triple', 'Cuadruple'))
+   NOT NULL DEFAULT 'Simple';
+ 
+CREATE DOMAIN esquema_grupo1_alt.domItemEvaluado AS VARCHAR(20)
+   CHECK (VALUE IN ('COCINA', 'BANIO', 'LIVING', 'BALCON', 'LIMPIEZA', 'PUNTUALIDAD', 'RUIDO'));
+ 
+CREATE DOMAIN esquema_grupo1_alt.ingresotexto AS VARCHAR(300)
+   DEFAULT 'SIN DESCRIPCION';
+ 
+CREATE DOMAIN esquema_grupo1_alt.precioNoche AS INTEGER
+   CHECK (VALUE >= 20000)
+   NOT NULL DEFAULT 20000;
+ 
+CREATE DOMAIN esquema_grupo1_alt.domEstado AS VARCHAR(20)
+   CHECK (VALUE IN ('Pendiente', 'Aceptada', 'Confirmada', 'Rechazada', 'Cancelada'));
+ 
+CREATE DOMAIN esquema_grupo1_alt.montopagado AS BOOLEAN
+   DEFAULT FALSE;
+ 
+CREATE DOMAIN esquema_grupo1_alt.inicioReserva AS DATE
+   CHECK (VALUE >= CURRENT_DATE);
+ 
+CREATE DOMAIN esquema_grupo1_alt.edad AS DATE
+   CHECK (EXTRACT(YEAR FROM AGE(CURRENT_DATE, VALUE)) >= 21);
+
